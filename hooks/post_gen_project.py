24b63ee69496383973a58f3ec02703c696584e49
Alexey ALERT Rubasheff <ALERTua@users.noreply.github.com>
2020-07-26 12:47:20 +0300

    Update post_gen_project.py
    windows precaution

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,4 +3,5 @@ import os
 # Rename root repo directory to use `-` instead of `_`.
 repo_dir = "{{cookiecutter.domain}}"
 new_repo_dir = repo_dir.replace("_", "-")
-os.rename(os.path.join("..", repo_dir), os.path.join("..", new_repo_dir))
+if repo_dir != new_repo_dir:
+    os.rename(os.path.join("..", repo_dir), os.path.join("..", new_repo_dir))
